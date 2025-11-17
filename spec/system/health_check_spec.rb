ef8afbd0c9c7ff250d366b935ee7bdb71ce0db97
James Thompson <james@thomps.onl>
2025-11-16 23:39:00 -0500

    Fix Health Check system test.

--- spec/system/health_check_spec.rb
+++ spec/system/health_check_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe 'Health Check', type: :system do
   it 'displays the health check page' do
     visit rails_health_check_path
 
-    expect(page).to have_http_status(:ok)
+    expect(page).to have_content("")
   end
 end
